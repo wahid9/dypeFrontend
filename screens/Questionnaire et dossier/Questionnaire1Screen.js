c724ea6383b3da20ef5175ea176a44fa3ec72649
Hasni Fodeilla <Hasni-fodeilla99@hotmail.com>
2020-03-02 17:30:09 +0100

    hasni commit

--- screens/Questionnaire et dossier/Questionnaire1Screen.js
+++ screens/Questionnaire et dossier/Questionnaire1Screen.js
@@ -7,7 +7,7 @@ function Questionnaire1Screen() {
     <View style={styles.container}>
 
       <Image
-        source={require('./assets/DYPE_noir_blanc.png')}
+        source={require('../../assets/DYPE_noir_blanc.png')}
         style={{width: 125, height: 65, marginTop: '15%', marginBottom: '10%'}}
       />
 
