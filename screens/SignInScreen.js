7496cb08c1f76eed17d4b7ab39ef8a730f8dd49e
Valentin Bourgeois <v.bourgeois90@gmail.com>
2020-03-02 14:23:42 +0100

    1er commit front VB

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>APP SCREEN</Text>
+      <Text>SIGN IN</Text>
     </View>
   );
 }
