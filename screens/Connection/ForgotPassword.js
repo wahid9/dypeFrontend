6c56c4f5d5cba9c53b423f9dacd9a26b34f4d685
Hasni Fodeilla <Hasni-fodeilla99@hotmail.com>
2020-03-06 15:40:43 +0100

    update page forgot

--- screens/Connection/ForgotPassword.js
+++ screens/Connection/ForgotPassword.js
@@ -4,6 +4,40 @@ import {Button, Input} from 'react-native-elements';
 
 function ForgotPassword({navigation}) {
 
+  const [email,setEmail]= useState("");
+  const [mdp,setMdp]= useState("");
+  const [mdpConfirm,setMdpConfirm]= useState("");
+
+  var Btn;
+  if(email == ''){
+    Btn = <Button
+    buttonStyle= {{backgroundColor: "#125CE0",borderRadius:5,paddingLeft:65,paddingRight:65}}
+    title="Confirmer"
+    string = "#79d279" 
+    onPress = {()=> Alert.alert("Remplissez vos champs de saisie"," Veuillez remplir tous les champs de saisie")}/>
+  }else if(mdp== ''){
+    Btn = <Button
+    buttonStyle= {{backgroundColor: "#125CE0",borderRadius:5,paddingLeft:65,paddingRight:65}}
+    title="Confirmer"
+    string = "#79d279" 
+    onPress = {()=> Alert.alert("Remplissez vos champs de saisie", "Veuillez remplir tous les champs de saisie")}
+/>
+  }else if (mdpConfirm == ""){
+    Btn = <Button
+    buttonStyle= {{backgroundColor: "#125CE0",borderRadius:5,paddingLeft:65,paddingRight:65}}
+    title="Confirmer"
+    string = "#79d279" 
+    onPress = {()=> Alert.alert("Remplissez vos champs de saisie", "Veuillez remplir tous les champs de saisie")}
+/>
+  }else{
+  Btn =  <Button
+          buttonStyle= {{backgroundColor: "#125CE0",borderRadius:5,paddingLeft:65,paddingRight:65}}
+          title="Confirmer"
+          string = "#79d279" 
+          onPress={() => navigation.navigate('SignIn') }
+      />
+  }
+
   return (
     <ImageBackground source={require('../../assets/picture.jpg')}  style={styles.container}>
      <Image
@@ -14,20 +48,22 @@ function ForgotPassword({navigation}) {
     <Input containerStyle = {{marginBottom: 25, width: '70%'}} 
         inputStyle={{ backgroundColor:"white",borderRadius:5,paddingTop:2, opacity:0.7}}
         placeholder='Email'
+        onChangeText = {(value)=> setEmail(value)}
+        value = {email}
         />
     <Input containerStyle = {{marginBottom: 25, width: '70%'}} 
       inputStyle={{ backgroundColor:"white",borderRadius:5,paddingTop:2, opacity:0.7}}
       placeholder='Nouveau mot de passe'
+      onChangeText = {(value)=> setMdp(value)}
+      value = {mdp}
     />
     <Input containerStyle = {{marginBottom: 25, width: '70%'}} 
         inputStyle={{ backgroundColor:"white",borderRadius:5,paddingTop:2, opacity:0.7}}
         placeholder='Confirmation mot de passe'
+        onChangeText = {(value)=> setMdpConfirm(value)}
+        value = {mdpConfirm}
         />
-    <Button
-        buttonStyle= {{backgroundColor: "#125CE0",borderRadius:5,paddingLeft:65,paddingRight:65}}
-        title="Confirmer"
-        string = "#79d279" 
-        onPress = {()=> Alert.alert("Remplissez vos champs de saisie", "Veuillez remplir vos différents champs de saisie")}/>
+        {Btn}
       <KeyboardAvoidingView behavior = "padding" enabled>
       </KeyboardAvoidingView>
     </ImageBackground>
